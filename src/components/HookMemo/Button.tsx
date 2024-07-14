6baa92a56dd225dd9964332e31e51451381866d2
Hideki Ambe <ykc@daycrift.net>
2024-07-14 20:23:28 +0900

    feat: Add new HookMemo component with memoized function
    This commit adds a new component called HookMemo, made up of three child components: Button, Counter, and a main Component. This main Component utilizes the useState and useMemo React hooks. The useMemo hook is specifically used to memorize a heavy processing function result to optimize performance, ensuring the function doesn't run at every render unless its dependencies change. A story for the new component in Storybook is also added.

---
+++ src/components/HookMemo/Button.tsx
@@ -0,0 +1,17 @@
+
+type ButtonProps = {
+  id: string;
+  handleClick: () => void;
+  children: React.ReactNode;
+}
+
+const Button = (props: ButtonProps) => {
+  const { id, handleClick, children } = props;
+
+  console.log(`Button rendered id: ${id}`)
+
+  return (
+    <button key={id} onClick={handleClick}>{children}</button>
+  )
+}
+export default Button;
---
+++ src/components/HookMemo/Counter.tsx
@@ -0,0 +1,16 @@
+
+type CounterProps = {
+  id: string;
+  value: number;
+}
+
+const Counter = (props: CounterProps) => {
+  const { id, value } = props;
+
+  console.log(`Counter is rendered id: ${id}`);
+
+  return (
+    <p>現在値: {value}</p>
+  )
+}
+export default Counter;
